1bedd99d1a3ca8a7835f82f2863ed64bcbfedbb6
coolclear <yz78@rice.edu>
2018-03-26 14:35:27 +0800

    debug

--- CNN/GBP_Denoising/prepare_data.py
+++ CNN/GBP_Denoising/prepare_data.py
@@ -1,11 +1,14 @@
 import sys
 sys.path.append('/home/yang/Research/CNN/')
 sys.path.append('/home/yang/Research/CNN/Prepare_Model')
+sys.path.append('/home/yang/Research/CNN/Tools')
 
 from keras.datasets import cifar10
 import numpy as np
 import tensorflow as tf
 from Prepare_Model import prepare_GBP_small_CNN
+from Plot import simple_plot
+import pickle as pkl
 
 
 # length of the logits vector
@@ -22,7 +25,6 @@ def GBP_Reconstruction(image):
 
     # generate a random index
     random_index = np.random.choice(output_dim, 1)
-    print(random_index)
 
     # the tf tensor to calculate the GBP visualization
     gbp_tensor = tf.gradients(model.logits[:, random_index], model.layers_dic['images'])[0]
@@ -39,6 +41,18 @@ def GBP_Reconstruction(image):
 # each example to its corresponding GBP reconstruction
 X_train_gbp = [GBP_Reconstruction(img) for img in X_train_ori]
 X_test_gbp =  [GBP_Reconstruction(img) for img in X_test_ori]
+
+# save to pickle
+f = open('./../../../Data/{}.pkl'.format('CIFAR10_GBP'), 'wb')
+pkl.dump((X_train_gbp, y_train), f, -1)
+pkl.dump((X_test_gbp, y_test), f, -1)
+f.close()
+
+# visualize some images
+random_indices = np.random.choice(50000, 10)
+for i in random_indices:
+    simple_plot(X_train_ori[i], '{}_ori.png'.format(i), 'viz')
+    simple_plot(X_train_gbp[i], '{}_gbp.png'.format(i), 'viz')
 
 
 
