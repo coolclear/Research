0ef9a8df159279718388198717eaa90efdb933f3
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-03-31 06:22:54 -0500

    modify

--- CNN/GBP_Denoising/train.py
+++ CNN/GBP_Denoising/train.py
@@ -5,7 +5,6 @@
 ## This program is licenced under the BSD 2-Clause licence,
 ## contained in the LICENCE file in this directory.
 
-from setup_mnist import MNIST, MNISTModel
 from setup_cifar import CIFAR, CIFARModel
 from keras.optimizers import SGD
 from keras.callbacks import LearningRateScheduler
@@ -15,6 +14,7 @@ import numpy as np
 import tensorflow as tf
 
 def train(data, Model, file_name, num_epochs=50, batch_size=128, init=None):
+
     def fn(correct, predicted):
         return tf.nn.softmax_cross_entropy_with_logits(labels=correct,
                                                        logits=predicted)
@@ -24,28 +24,20 @@ def train(data, Model, file_name, num_epochs=50, batch_size=128, init=None):
 
     def get_lr(epoch):
         return base_lr*(.5**(epoch/num_epochs*10))
+
     sgd = SGD(lr=0.00, momentum=0.9, nesterov=False)
     schedule= LearningRateScheduler(get_lr)
 
     model.compile(loss=fn,
                   optimizer=sgd,
                   metrics=['accuracy'])
-    
-    if Model == MNISTModel:
-        datagen = ImageDataGenerator(
-            rotation_range=0,
-            width_shift_range=0.0,
-            height_shift_range=0.0,
-            horizontal_flip=False)
-        base_lr = 0.1
-    else:
-        datagen = ImageDataGenerator(
-            rotation_range=10,
-            width_shift_range=0.1,
-            height_shift_range=0.1,
-            horizontal_flip=True)
-        base_lr = 0.1
 
+    datagen = ImageDataGenerator(
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True)
+    base_lr = 0.1
 
     datagen.fit(data.train_data)
 
@@ -65,5 +57,4 @@ def train(data, Model, file_name, num_epochs=50, batch_size=128, init=None):
     return model
 
 if __name__ == "__main__":
-    train(MNIST(), MNISTModel, "models/mnist", num_epochs=30)
     train(CIFAR(), CIFARModel, "models/cifar", num_epochs=300)
