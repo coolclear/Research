9c3d75454cc2703d03dba7fd745268b07d0b9344
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-03-31 21:42:31 -0500

    debug

--- CNN/GBP_Denoising/interchangeability.py
+++ CNN/GBP_Denoising/interchangeability.py
@@ -1,21 +1,48 @@
-# trained on ORI, test on GBP
-if tag == "ORI":
+import numpy as np
+from setup_cifar import CIFAR, CIFARModel
+
+def main():
+
+    tag = "GBP"
+    model = CIFARModel("Models/CIFAR10_{}".format(tag)).model
     data_gbp = CIFAR("GBP")
-
-    print('Trained on ORI and Test on GBP(Train):',
-          np.mean(np.argmax(model.predict(data_gbp.train_data), axis=1) == np.argmax(data_gbp.train_labels,
-                                                                                     axis=1)))
-
-    print('Trained on ORI and Test on GBP(Test):',
-          np.mean(np.argmax(model.predict(data_gbp.test_data), axis=1) == np.argmax(data_gbp.test_labels, axis=1)))
-
-# trained on GBP, test on ORI
-if tag == "GBP":
     data_ori = CIFAR("ORI")
 
-    print('Trained on GBP and Test on ORI(Train):',
-          np.mean(np.argmax(model.predict(data_ori.train_data), axis=1) == np.argmax(data_ori.train_labels,
-                                                                                     axis=1)))
-    print('Trained on GBP and Test on ORI(Test):',
-          np.mean(
-              np.argmax(model.predict(data_ori.test_data), axis=1) == np.argmax(data_ori.test_labels, axis=1)))
+    # trained on ORI, test on GBP
+    if tag == "ORI":
+
+        print('Training Accuracy on ORI:',
+              np.mean(np.argmax(model.predict(data_ori.train_data), axis=1) == np.argmax(data_ori.train_labels,
+                                                                                         axis=1)))
+
+        print('Testing Accuracy on ORI:',
+              np.mean(np.argmax(model.predict(data_ori.test_data), axis=1) == np.argmax(data_ori.test_labels,
+                                                                                         axis=1)))
+
+        print('Trained on ORI and Test on GBP(Train):',
+              np.mean(np.argmax(model.predict(data_gbp.train_data), axis=1) == np.argmax(data_gbp.train_labels,
+                                                                                         axis=1)))
+
+        print('Trained on ORI and Test on GBP(Test):',
+              np.mean(np.argmax(model.predict(data_gbp.test_data), axis=1) == np.argmax(data_gbp.test_labels, axis=1)))
+
+    # trained on GBP, test on ORI
+    if tag == "GBP":
+
+        print('Training Accuracy on GBP:',
+              np.mean(np.argmax(model.predict(data_gbp.train_data), axis=1) == np.argmax(data_gbp.train_labels,
+                                                                                         axis=1)))
+
+        print('Testing Accuracy on GBP:',
+              np.mean(np.argmax(model.predict(data_gbp.test_data), axis=1) == np.argmax(data_gbp.test_labels,
+                                                                                        axis=1)))
+        
+        print('Trained on GBP and Test on ORI(Train):',
+              np.mean(np.argmax(model.predict(data_ori.train_data), axis=1) == np.argmax(data_ori.train_labels,
+                                                                                         axis=1)))
+        print('Trained on GBP and Test on ORI(Test):',
+              np.mean(
+                  np.argmax(model.predict(data_ori.test_data), axis=1) == np.argmax(data_ori.test_labels, axis=1)))
+
+if __name__ == "__main__":
+    main()
