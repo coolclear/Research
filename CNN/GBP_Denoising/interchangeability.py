5821e7e0e9f114090597e9ef01f3416647f50ba0
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-03-31 22:36:49 -0500

    debug

--- CNN/GBP_Denoising/interchangeability.py
+++ CNN/GBP_Denoising/interchangeability.py
@@ -11,8 +11,16 @@ def main():
     # trained on ORI, test on GBP
     if tag == "ORI":
 
+        print('Training Accuracy on ORI:',
+              np.mean(np.argmax(model.predict(data_ori.train_data), axis=1) == np.argmax(data_ori.train_labels,
+                                                                                         axis=1)))
+
         print('Testing Accuracy on ORI:',
               np.mean(np.argmax(model.predict(data_ori.test_data), axis=1) == np.argmax(data_ori.test_labels,
+                                                                                         axis=1)))
+
+        print('Trained on ORI and Test on GBP(Train):',
+              np.mean(np.argmax(model.predict(data_gbp.train_data), axis=1) == np.argmax(data_gbp.train_labels,
                                                                                          axis=1)))
 
         print('Trained on ORI and Test on GBP(Test):',
@@ -20,6 +28,10 @@ def main():
 
     # trained on GBP, test on ORI
     if tag == "GBP":
+
+        print('Training Accuracy on GBP:',
+              np.mean(np.argmax(model.predict(data_gbp.train_data), axis=1) == np.argmax(data_gbp.train_labels,
+                                                                                         axis=1)))
 
         print('Testing Accuracy on GBP:',
               np.mean(np.argmax(model.predict(data_gbp.test_data), axis=1) == np.argmax(data_gbp.test_labels,
