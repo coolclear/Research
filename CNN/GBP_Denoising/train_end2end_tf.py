8306ef2118f89180ae2006bbff3504a45194f8c0
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-04-03 23:00:49 -0500

    debug

--- CNN/GBP_Denoising/train_end2end_tf.py
+++ CNN/GBP_Denoising/train_end2end_tf.py
@@ -5,8 +5,8 @@ import sys
 sys.path.append('/home/yang/Research/CNN/')
 from Prepare_Model import prepare_GBPdenoising_end2end
 
-
 import tensorflow as tf
+import keras
 from keras.datasets import cifar10
 
 
@@ -15,6 +15,9 @@ def main():
     num_epochs = 100
     batch_size = 128
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
 
     tf_model = prepare_GBPdenoising_end2end()
     input_pl = tf_model.inputs
