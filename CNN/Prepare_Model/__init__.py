1c54c9c92c4597c5a908918bcf531115607d14b4
coolclear <yz78@rice.edu>
2018-04-08 18:10:40 +0800

    debug

--- CNN/Prepare_Model/__init__.py
+++ CNN/Prepare_Model/__init__.py
@@ -187,7 +187,6 @@ def prepare_GBPdenoising_end2end(sess=None, trainable=False, saved=None):
         model.init(sess)
 
     if saved != None and sess != None:
-        model.init(sess)
         saver = tf.train.Saver()
         saver.restore(sess, saved)
         print('Trained weights are restored ... ')
