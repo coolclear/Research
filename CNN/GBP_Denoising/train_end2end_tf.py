e62442ca419f87c668e6828347a2768f28e39eab
coolclear <yz78@rice.edu>
2018-04-08 19:40:31 +0800

    debug

--- CNN/GBP_Denoising/train_end2end_tf.py
+++ CNN/GBP_Denoising/train_end2end_tf.py
@@ -15,7 +15,7 @@ def main():
 
     trainable = False
     num_classes = 10
-    num_epochs = 5
+    num_epochs = 1
     batch_size = 64
 
     ########################################## Prepare the Data ########################################################
