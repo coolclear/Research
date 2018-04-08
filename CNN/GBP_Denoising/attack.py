e7c7dd1db2dfc47862a344fc0f2eae5bc9b1d469
coolclear <yz78@rice.edu>
2018-04-08 20:43:30 +0800

    debug

--- CNN/GBP_Denoising/attack.py
+++ CNN/GBP_Denoising/attack.py
@@ -83,7 +83,7 @@ def attack_one_image(image, name, label, attack_type, criterion, fool_model):
                 attack = foolbox.attacks.FGSM(fool_model, criterion=criterion)
 
             # attack happens here
-            adversarial = attack(image, label)
+            adversarial = attack(image, label=label)
 
             preds_adv = fool_model.predictions(adversarial)
             label_pre_adv = np.argmax(preds_adv)
