72b32aa11c96b301c76896659fe602fbabeb60a0
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-04-24 00:17:57 -0500

    generate advs on resnet

--- CNN/GBP_Denoising/attack.py
+++ CNN/GBP_Denoising/attack.py
@@ -190,7 +190,7 @@ def attack_one_image(image, name, label, attack_type, fool_model):
 
                     print('The attack is successed!')
 
-                    simple_plot(adversarial, 'ADV' + name, './Adversarial_Examples/{}/'.format(attack_type))
+                    simple_plot(adversarial.astype(int), 'ADV' + name, './Adversarial_Examples/{}/'.format(attack_type))
 
                     print('Saved!')
 
