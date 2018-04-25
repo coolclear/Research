664c095bbf70c64ccd0cda3071da27d14a2127bc
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-04-24 23:58:16 -0500

    attack experiment

--- CNN/GBP_Denoising/stable.py
+++ CNN/GBP_Denoising/stable.py
@@ -32,13 +32,13 @@ def main():
 
     with tf.Session() as sess:
 
-        # pure Resnet
-        tf_model = prepare_resnet(sess=sess,
-                                  load_weights='./Models/LearningCurve_Resnet_Trainable_{}.ckpt'.format(trainable))
+        # # pure Resnet
+        # tf_model = prepare_resnet(sess=sess,
+        #                           load_weights='./Models/LearningCurve_Resnet_Trainable_{}.ckpt'.format(trainable))
 
-        # tf_model = prepare_GBPdenoising_end2end(sess=sess,
-        #                                         trainable=trainable,
-        #                                         saved='./Models/LearningCurve_End2End_Trainable_{}.ckpt'.format(trainable))
+        tf_model = prepare_GBPdenoising_end2end(sess=sess,
+                                                trainable=trainable,
+                                                saved='./Models/LearningCurve_End2End_Trainable_{}.ckpt'.format(trainable))
 
         input_pl = tf_model.inputs
         logits = tf_model.logits
