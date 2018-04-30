701fb9b1e9357446f3e8e938f983a0fa777bb5e6
coolclear <Yang@DESKTOP-KG6VURJ.localdomain>
2018-04-30 02:42:18 -0500

    paper figure

--- CNN/Tools/Plot.py
+++ CNN/Tools/Plot.py
@@ -50,11 +50,10 @@ def grid_plot(grid_shape, image_list, title, save_dir, save_name):
     rows = grid_shape[0]
     columns = grid_shape[1]
 
-    plt.tight_layout()
     plt.title(title, loc='left')
     fig = plt.figure()
 
-    gs = gridspec.GridSpec(rows, columns, wspace=0.2, hspace=0.)
+    gs = gridspec.GridSpec(rows, columns, wspace=0., hspace=0.)
 
     for i in range(rows):
         for j in range(columns):
