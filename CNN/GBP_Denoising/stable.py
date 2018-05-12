b391bdb5e3a978965f55fa9e8f94a0eb1e236977
coolclear <yz78@rice.edu>
2018-05-12 18:00:54 -0500

    5.3

--- CNN/GBP_Denoising/stable.py
+++ CNN/GBP_Denoising/stable.py
@@ -1,6 +1,8 @@
 import os
 import sys
 sys.path.append('/home/yang/Research/CNN/')
+
+from Prepare_Data import pickle_load, prepare_CIFAR10, prepare_CIFAR100, prepare_SVHN
 from Prepare_Model import prepare_GBPdenoising_end2end, prepare_resnet
 sys.path.append('/home/yang/Research/CNN/Tools')
 from Plot import simple_plot
@@ -44,7 +46,8 @@ def main():
 
         # pure Resnet
         tf_model = prepare_resnet(sess=sess,
-                                  load_weights='./Models/LearningCurve_Resnet_Trainable_{}.ckpt'.format(trainable))
+                                  load_weights='./Models/CIFAR10_Resnet.ckpt',
+                                  num_classes=num_classes)
 
         # tf_model = prepare_GBPdenoising_end2end(sess=sess,
         #                                         trainable=trainable,
@@ -53,61 +56,63 @@ def main():
         input_pl = tf_model.inputs
         logits = tf_model.logits
 
-        for tag in ADVs_End2End:
+        # for tag in ADVs_End2End:
 
-            print(tag)
+            # print(tag)
 
-            if tag == "ORI":
-                (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-                # x_test = x_test[:1000]
-                # y_test = y_test[:1000]
-            else:
-                with open('{}.pkl'.format(tag), 'rb') as file:
-                    (x_test, y_test) = pkl.load(file)
+            # if tag == "ORI":
+            #     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+            #     # x_test = x_test[:1000]
+            #     # y_test = y_test[:1000]
+            # else:
+            #     with open('{}.pkl'.format(tag), 'rb') as file:
+            #         (x_test, y_test) = pkl.load(file)
+            #
+            # print("Number of examples = {}".format(len(x_test)))
+            #
+            # if len(x_test) == 0:
+            #     continue
 
-            print("Number of examples = {}".format(len(x_test)))
+        (x_train, y_train), (x_test, y_test) = prepare_CIFAR10()
 
-            if len(x_test) == 0:
-                continue
+        # predict one by one
+        # for each, we predict for N times to test the model stability
+        # set N = 1 for normal prediction
+        # check the testing accuracy
+        test_accu_single = 0.
+        test_accu_vote = 0.
+        var = 0.
+        times = 50
+        stable = 0.
+        for index, image in enumerate(x_test):
 
-            # predict one by one
-            # for each, we predict for N times to test the model stability
-            # set N = 1 for normal prediction
-            # check the testing accuracy
-            test_accu_single = 0.
-            test_accu_vote = 0.
-            var = 0.
-            times = 50
-            stable = 0.
-            for index, image in enumerate(x_test):
+            if index % 500 == 0:
+                print(index)
 
-                if index % 500 == 0:
-                    print(index)
+            label = y_test[index]
 
-                label = y_test[index]
+            batch_image = np.expand_dims(image, axis=0)
+            # prediction
+            logit_vals = []
+            for step in range(times):
+                logit_vals.append(np.argmax(sess.run(logits, feed_dict={input_pl: batch_image})[0]))
 
-                batch_image = np.expand_dims(image, axis=0)
-                # prediction
-                logit_vals = []
-                for step in range(times):
-                    logit_vals.append(np.argmax(sess.run(logits, feed_dict={input_pl: batch_image})[0]))
+            diffs = len(np.unique(logit_vals))
+            var += diffs
 
-                diffs = len(np.unique(logit_vals))
-                var += diffs
+            if diffs == 1:
+                stable += 1
 
-                if diffs == 1:
-                    stable += 1
+            if logit_vals[0] == label:
+                test_accu_single += 1
 
-                if logit_vals[0] == label:
-                    test_accu_single += 1
+            if np.bincount(logit_vals).argmax() == label:
+                test_accu_vote += 1
 
-                if np.bincount(logit_vals).argmax() == label:
-                    test_accu_vote += 1
-
-            print("Single Test Accuracy = {:.4f}".format(test_accu_single / len(x_test)))
-            print("Vote Test Accuracy = {:.4f}".format(test_accu_vote / len(x_test)))
-            print("Stability = {:.4f}".format(var / len(x_test)))
-            print("Percentage = {:.4f}".format(stable / len(x_test)))
+        print("Single Test Accuracy = {:.4f}".format(test_accu_single / len(x_test)))
+        print("Vote Test Accuracy = {:.4f}".format(test_accu_vote / len(x_test)))
+        print("Stability = {:.4f}".format(var / len(x_test)))
+        print("Percentage = {:.4f}".format(stable / len(x_test)))
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
