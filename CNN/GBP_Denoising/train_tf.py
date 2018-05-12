18e4ddd740229710bac76bc28767e41364a2a4c9
coolclear <yz78@rice.edu>
2018-05-11 21:52:42 -0500

    svhn gbp0

--- CNN/GBP_Denoising/train_tf.py
+++ CNN/GBP_Denoising/train_tf.py
@@ -14,13 +14,13 @@ def main():
 
     model_type = 'Resnet'
 
-    num_classes = 100
+    num_classes = 10
     num_epochs = 300
     batch_size = 64
 
     ########################################## Prepare the Data ########################################################
 
-    (x_train, y_train), (x_test, y_test) = pickle_load("./", "CIFAR100_GBP_0.pkl")
+    (x_train, y_train), (x_test, y_test) = pickle_load("./", "SVHN_GBP_0.pkl")
 
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -79,8 +79,8 @@ def main():
 
     # TensorBoard for the recording
     merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter('TensorBoard/CIFAR100-GBP0/{}/Train'.format(model_type))
-    test_writer = tf.summary.FileWriter('TensorBoard/CIFAR100-GBP0/{}/Test'.format(model_type))
+    train_writer = tf.summary.FileWriter('TensorBoard/SVHN-GBP0/{}/Train'.format(model_type))
+    test_writer = tf.summary.FileWriter('TensorBoard/SVHN-GBP0/{}/Test'.format(model_type))
 
     init = tf.global_variables_initializer() # initializer
 
@@ -153,7 +153,7 @@ def main():
 
         print(msg)
 
-        saver.save(sess, 'Models/CIFAR100-GBP0_{}.ckpt'.format(model_type))
+        saver.save(sess, 'Models/SVHN-GBP0_{}.ckpt'.format(model_type))
 
 if __name__ == "__main__":
     # setup the GPUs to use
