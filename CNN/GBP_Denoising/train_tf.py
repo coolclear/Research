7d82581e825a9fba915ee5bc56ee197f1bc6c71d
coolclear <yz78@rice.edu>
2018-05-13 00:12:21 -0500

    svhn end2end retrain

--- CNN/GBP_Denoising/train_tf.py
+++ CNN/GBP_Denoising/train_tf.py
@@ -12,7 +12,7 @@ from keras.preprocessing.image import ImageDataGenerator
 
 def main():
 
-    model_type = 'Resnet'
+    model_type = 'End2End'
 
     num_classes = 10
     num_epochs = 300
@@ -20,7 +20,7 @@ def main():
 
     ########################################## Prepare the Data ########################################################
 
-    (x_train, y_train), (x_test, y_test) = prepare_CIFAR10()
+    (x_train, y_train), (x_test, y_test) = prepare_SVHN("./")
 
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -79,8 +79,8 @@ def main():
 
     # TensorBoard for the recording
     merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter('TensorBoard/CIFAR10/{}/Train'.format(model_type))
-    test_writer = tf.summary.FileWriter('TensorBoard/CIFAR10/{}/Test'.format(model_type))
+    train_writer = tf.summary.FileWriter('TensorBoard/SVHN/{}/Train'.format(model_type))
+    test_writer = tf.summary.FileWriter('TensorBoard/SVHN/{}/Test'.format(model_type))
 
     init = tf.global_variables_initializer() # initializer
 
@@ -153,7 +153,7 @@ def main():
 
         print(msg)
 
-        saver.save(sess, 'Models/CIFAR10_{}.ckpt'.format(model_type))
+        saver.save(sess, 'Models/SVHN_{}.ckpt'.format(model_type))
 
 if __name__ == "__main__":
     # setup the GPUs to use
