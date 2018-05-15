4e8e7ffc6e05369c7a893c15bcb1dfa01033f5f8
coolclear <yz78@rice.edu>
2018-05-14 20:19:21 -0500

    5.4

--- CNN/GBP_Denoising/attack_models.py
+++ CNN/GBP_Denoising/attack_models.py
@@ -49,21 +49,21 @@ def softmax_np(x, axis=None):
 def main():
 
     # load in the data
-    (x_train, y_train), (x_test, y_test) = prepare_CIFAR10()
+    (x_train, y_train), (x_test, y_test) = prepare_SVHN("./")
 
     L2_error = 16 * 32
     Linf_error = 16
 
     with tf.Session() as sess:
 
-        # # pure Resnet
-        # tf_model = prepare_resnet(sess=sess,
-        #                           load_weights='./Models/CIFAR10_Resnet.ckpt',
-        #                           num_classes=10)
+        # pure Resnet
+        tf_model = prepare_resnet(sess=sess,
+                                  load_weights='./Models/SVHN_Resnet.ckpt',
+                                  num_classes=10)
 
-        # End2End
-        tf_model = prepare_GBPdenoising_end2end(sess=sess,
-                                                saved='./Models/CIFAR10_End2End.ckpt')
+        # # End2End
+        # tf_model = prepare_GBPdenoising_end2end(sess=sess,
+        #                                         saved='./Models/CIFAR10_End2End.ckpt')
 
         input_pl = tf_model.inputs
         logits = tf_model.logits
@@ -94,22 +94,22 @@ def main():
                         adv_x_test_L2.append(adv)
                         adv_y_test_L2.append(y_test[index])
                         simple_plot(adv.astype(int), 'ADV' + 'TEST_{}'.format(index),
-                                    './Adversarial_Examples/CIFAR10/End2End_off/L2/{}/'.format(attack_type))
+                                    './Adversarial_Examples/SVHN/Resnet_off/L2/{}/'.format(attack_type))
 
 
                     if Linf <= Linf_error:
                         adv_x_test_Linf.append(adv)
                         adv_y_test_Linf.append(y_test[index])
                         simple_plot(adv.astype(int), 'ADV' + 'TEST_{}'.format(index),
-                                    './Adversarial_Examples/CIFAR10/End2End_off/Linf/{}/'.format(attack_type))
+                                    './Adversarial_Examples/SVHN/Resnet_off/Linf/{}/'.format(attack_type))
 
 
             # save to pickle
-            f = open('./ADVs_CIFAR10_End2End_off_L2_{}.pkl'.format(attack_type), 'wb')
+            f = open('./ADVs_SVHN_Resnet_off_L2_{}.pkl'.format(attack_type), 'wb')
             pkl.dump((adv_x_test_L2, adv_y_test_L2), f, -1)
             f.close()
 
-            f = open('./ADVs_CIFAR10_End2End_off_Linf_{}.pkl'.format(attack_type), 'wb')
+            f = open('./ADVs_SVHN_Resnet_off_Linf_{}.pkl'.format(attack_type), 'wb')
             pkl.dump((adv_x_test_Linf, adv_y_test_Linf), f, -1)
             f.close()
 
