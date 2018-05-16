a46bb32774527a6422a68e17d87ae432538c0dbd
coolclear <yz78@rice.edu>
2018-05-15 23:45:14 -0500

    salmap

--- CNN/GBP_Denoising/attack_models.py
+++ CNN/GBP_Denoising/attack_models.py
@@ -181,7 +181,7 @@ def attack_one_image(image, name, label, attack_type, fool_model):
                 attack = foolbox.attacks.FGSM(fool_model)
 
             # attack happens here
-            adversarial = attack(image, int(label), theta=7)
+            adversarial = attack(image, int(label), theta=16)
 
             """
             Notice that for a given input image if we run the model multiple times the predictions
