d958b5b3d208ba3b3712572b7623201103e767b4
coolclear <yz78@rice.edu>
2018-05-29 16:27:29 -0700

    debug

--- CNN/GBP_Denoising/attack_models.py
+++ CNN/GBP_Denoising/attack_models.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 sys.path.append('/home/yang/Research/CNN/')
 from Prepare_Data import pickle_load, prepare_CIFAR10, prepare_CIFAR100, prepare_SVHN
 from Prepare_Model import prepare_GBP_End2End, prepare_Resnet
@@ -15,15 +16,23 @@ from cleverhans.model import CallableModelWrapper
 from cleverhans.utils_tf import model_eval
 from cleverhans.attacks import FastGradientMethod
 
-output_dim = 10
-input_dim = 32
-
 eval_params = {'batch_size': 128}
-size = 100
+size = 1000
 
-def main():
+def main(type="Resnet", dataset="CIFAR10"):
 
-    (x_train, y_train), (x_test, y_test) = prepare_CIFAR10()
+    if dataset == 'CIAFR10':
+        (x_train, y_train), (x_test, y_test) = prepare_CIFAR10()
+        num_classes = 10
+        input_dim = 32
+    elif dataset == 'CIFAR100':
+        (x_train, y_train), (x_test, y_test) = prepare_CIFAR100()
+        num_classes = 100
+        input_dim = 32
+    else:
+        (x_train, y_train), (x_test, y_test) = prepare_SVHN("./")
+        num_classes = 10
+        input_dim = 32
 
     with tf.Session() as sess:
 
@@ -31,18 +40,38 @@ def main():
         x = tf.placeholder(tf.float32, [None, input_dim, input_dim, 3])
         y = tf.placeholder(tf.float32, [None, 1])
 
-        _, tf_model = \
-            prepare_Resnet(output_dim, inputT=x, checkpoint_dir="./Models", reuse=False,
-                           sess=sess)
+        # Model/Graph
+        if type == 'End2End':
+            _, tf_model = \
+                prepare_GBP_End2End(num_classes, inputT=x, checkpoint_dir="./{}_{}".format(dataset, type), sess=sess)
+        else:
+            _, tf_model = \
+                prepare_Resnet(num_classes, inputT=x, checkpoint_dir="./{}_{}".format(dataset, type), sess=sess)
 
         # create an attackable model for the cleverhans lib
         # we are doing a wrapping
-        model = CallableModelWrapper(lambda input: tf_model.logits, 'logits')
+        model = CallableModelWrapper(lambda ph_of_ph: tf_model.logits, 'logits')
         attack = FastGradientMethod(model, sess=sess)
         adv_x = attack.generate(x)
 
-        sess.run(adv_x, feed_dict={x: x_test[:100]})
+        # this is where we actually generate the adversarial examples
+        adv_vals = sess.run(adv_x, feed_dict={x: x_test[:size]})
+
+        accuracy = model_eval(sess, x, y, tf_model.logits, x_test[:size], y_test[:size],
+                              args=eval_params)
+        print('Test accuracy on normal examples: %0.4f' % accuracy)
+
+        accuracy = model_eval(sess, x, y, tf_model.logits, adv_vals, y_test[:size],
+                              args=eval_params)
+        print('Test accuracy on adversarial examples: %0.4f' % accuracy)
 
 if __name__ == '__main__':
+
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    main()
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-t", "--type", required=True, help="Resnet or End2End")
+    ap.add_argument("-d", "--dataset", required=True, help="CIFAR10, CIFAR100 or SVHN")
+    args = vars(ap.parse_args())
+
+    main(type=args["type"], dataset=["dataset"])
--- CNN/GBP_Denoising/train_tf.py
+++ CNN/GBP_Denoising/train_tf.py
@@ -148,7 +148,7 @@ def train(dataset, model_type, lr=1e-3, num_epochs=2, batch_size=64):
                     test_writer.add_summary(summary, b + e * steps_per_epoch)
 
                     if e % 1 == 0: # save every 5 epoches
-                        saver.save(sess, 'Models/{}_{}'.format(dataset, model_type), global_step=b + e * steps_per_epoch)
+                        saver.save(sess, '{}_{}'.format(dataset, model_type), global_step=b + e * steps_per_epoch)
 
                     break
 
