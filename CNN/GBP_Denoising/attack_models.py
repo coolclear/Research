eaee4469cd01142f9b8025a5da6779f73585ffd5
coolclear <yz78@rice.edu>
2018-05-29 23:27:51 -0700

    debug

--- CNN/GBP_Denoising/attack_models.py
+++ CNN/GBP_Denoising/attack_models.py
@@ -50,20 +50,20 @@ def main(type="Resnet", dataset="CIFAR10"):
 
         # create an attackable model for the cleverhans lib
         # we are doing a wrapping
-        model = CallableModelWrapper(lambda ph_of_ph: tf_model.logits, 'logits')
-        attack = FastGradientMethod(model, sess=sess)
-        adv_x = attack.generate(x)
+        # model = CallableModelWrapper(lambda ph_of_ph: tf_model.logits, 'logits')
+        # attack = FastGradientMethod(model, sess=sess)
+        # adv_x = attack.generate(x)
 
         # this is where we actually generate the adversarial examples
-        adv_vals = sess.run(adv_x, feed_dict={x: x_test[:size]})
+        # adv_vals = sess.run(adv_x, feed_dict={x: x_test[:size]})
 
         accuracy = model_eval(sess, x, y, tf_model.logits, x_test[:size], y_test[:size],
                               args=eval_params)
         print('Test accuracy on normal examples: %0.4f' % accuracy)
 
-        accuracy = model_eval(sess, x, y, tf_model.logits, adv_vals, y_test[:size],
-                              args=eval_params)
-        print('Test accuracy on adversarial examples: %0.4f' % accuracy)
+        # accuracy = model_eval(sess, x, y, tf_model.logits, adv_vals, y_test[:size],
+        #                       args=eval_params)
+        # print('Test accuracy on adversarial examples: %0.4f' % accuracy)
 
 if __name__ == '__main__':
 
